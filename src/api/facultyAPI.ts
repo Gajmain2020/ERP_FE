0a2534a63f920f1a302a40945aec76caa0cd1966
Gajendra Sahu <gajmain2020@gmail.com>
2025-03-14 00:19:26 +0530

    chore: added endpoint for teacher

--- src/api/facultyAPI.ts
+++ src/api/facultyAPI.ts
@@ -1,11 +1,25 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
+import { toast } from "sonner";
+
+const FacultyURL = "http://localhost:5500/api/v1/faculty";
 
 export async function LoginTeacherAPI(email: string, password: string) {
   try {
-    const res = await axios.post("/faculty/login", { email, password });
+    const res = await axios({
+      url: `${FacultyURL}/login`,
+      method: "POST",
+      data: { email, password },
+    });
     return res.data;
-  } catch (err) {
-    console.error(err);
-    return { success: false };
+  } catch (error: unknown) {
+    if (error instanceof AxiosError && error.response) {
+      toast.error(error.response.data.message);
+      return { success: false, message: error.response.data.message };
+    }
+    toast.error("Something went wrong. Please try again.");
+    return {
+      success: false,
+      message: "Something went wrong. Please try again.",
+    };
   }
 }
