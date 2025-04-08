2357dcd73bb5ee5b4838a5f845aec5aa9bd258e3
Gajendra Sahu <gajmain2020@gmail.com>
2025-04-08 17:15:36 +0530

    feat: ui and api integrated for enroll faculties

--- src/api/adminAPI.ts
+++ src/api/adminAPI.ts
@@ -133,3 +133,25 @@ export async function EnrollStudentsAPI(students: IStudent[]) {
     };
   }
 }
+
+export async function EnrollFacultiesAPI(faculties: IFaculty[]) {
+  try {
+    const res = await axios({
+      headers,
+      url: `${AdminURL}/enroll-multiple-students`,
+      method: "POST",
+      data: faculties,
+    });
+    return res.data;
+  } catch (error) {
+    if (error instanceof AxiosError && error.response) {
+      toast.error(error.response.data.message);
+      return { success: false, message: error.response.data.message };
+    }
+    toast.error("Something went wrong. Please try again.");
+    return {
+      success: false,
+      message: "Something went wrong. Please try again.",
+    };
+  }
+}
