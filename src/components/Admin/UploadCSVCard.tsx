372947e33ce4344277acac261373511e3319fa59
Gajendra Sahu <gajmain2020@gmail.com>
2025-04-08 16:35:40 +0530

    feat: ui with api integration done

--- src/components/Admin/UploadCSVCard.tsx
+++ src/components/Admin/UploadCSVCard.tsx
@@ -1,18 +1,24 @@
 import Papa from "papaparse";
 import { useState } from "react";
+import { toast } from "sonner";
 
+import { EnrollStudentsAPI } from "@/api/adminAPI";
 import {
   Card,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { IStudent } from "@/utils/types";
+import { addStudentSchema } from "@/utils/zodSchemas";
+import { Button } from "../ui/button";
 
 export default function UploadCSVCard() {
   const [students, setStudents] = useState<IStudent[]>([]);
+  const [loading, setLoading] = useState(false);
 
   const handleCSVUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
@@ -23,13 +29,53 @@ export default function UploadCSVCard() {
       skipEmptyLines: true,
       complete: function (results) {
         const parsedData = results.data;
-        console.log("Parsed CSV data:", parsedData);
         setStudents(parsedData as IStudent[]);
       },
       error: function (err) {
         console.error("Error parsing CSV:", err);
       },
     });
+  };
+
+  const handleAddStudents = async () => {
+    setLoading(true);
+    try {
+      const validStudents: IStudent[] = [];
+      let invalidCount = 0;
+
+      students.forEach((student: IStudent) => {
+        const result = addStudentSchema.safeParse(student);
+        if (result.success) {
+          validStudents.push(result.data);
+        } else {
+          invalidCount += 1;
+        }
+      });
+
+      if (invalidCount > 0) {
+        toast.error(`${invalidCount} student record(s) are invalid.`);
+      }
+
+      // Pass the valid students to the API for enrollment.
+
+      const res = await EnrollStudentsAPI(validStudents);
+
+      if (!res.success) {
+        toast.error(res.message);
+        return;
+      }
+
+      toast.success(res.message);
+
+      // Clear the students after successful enrollment
+      setStudents([]);
+    } catch (error) {
+      toast.error("An error occurred while adding students.");
+      console.error("Error adding students:", error);
+      return;
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
@@ -52,6 +98,14 @@ export default function UploadCSVCard() {
           </div>
         )}
       </CardContent>
+      <CardFooter className="flex justify-end">
+        <Button
+          onClick={handleAddStudents}
+          disabled={students.length === 0 || loading}
+        >
+          Add {students.length} students.
+        </Button>
+      </CardFooter>
     </Card>
   );
 }
