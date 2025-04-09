d2ef9da3a3f255fd46e58c0184bb295e66c0d4f1
Gajendra Sahu <gajmain2020@gmail.com>
2025-04-09 19:29:26 +0530

    sidebar fixed for admin

--- src/utils/nav-items.ts
+++ src/utils/nav-items.ts
@@ -221,11 +221,16 @@ export const adminNavItems = {
       path: "/time-table",
     },
   ],
-  TG: [
+  "Teacher Guardian": [
     {
       title: "Assign TG",
       icon: TbReportAnalytics,
       path: "/assign-tg",
+    },
+    {
+      title: "Add Students",
+      icon: TbReportAnalytics,
+      path: "/assign-student-to-tg",
     },
   ],
 };
